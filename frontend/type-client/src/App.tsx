f76cc53bd9f883b830b439209426fddf2a2bcb21
mstephe7 <matthew_stephens@brown.edu>
2022-05-07 23:41:41 -0400

    -m

--- frontend/type-client/src/App.tsx
+++ frontend/type-client/src/App.tsx
@@ -5,40 +5,49 @@ import Searcher from "./components/Searcher";
 
 var geniusLyricsAPI = require("genius-lyrics-api")
 
-// Here indices represents the search results for the program
+// Here indices represents the search results
 var lyrics = ' '
-var indices : any
+var indices  : any
 
 const options = {
-       title:  document.getElementById("search"),
+       //title:  document.getElementById("search"),
+       title:  'Halo',
        artist: ' ',
        apiKey: '3pTLkXn4VZpZXk3qJ61nkioYxvEaqzadxVGK1FNEN8-katQfRqngvvt1XzA06CaT', // Genius developer access token
        optimizeQuery: true // Setting this to true will optimize the query for best results
 }
 
 async function searchSongs() {
-    indices = await geniusLyricsAPI.searchSong(options)
+    await geniusLyricsAPI.searchSong(options)
     console.log(indices)
 }
 
-const searchResults = async () => {
-    await searchSongs()
-}
+await searchSongs()
 
 async function getLyrics() {
-    lyrics = await geniusLyricsAPI.getLyrics(indices[0].url)
+    await geniusLyricsAPI.getLyrics(indices[0].url)
 }
 
-const lyricsResults = async () => {
-    await getLyrics()
-}
+await getLyrics()
 
-  searchResults()
-  lyricsResults()
+// const searchResults = async () => {
+//     await searchSongs()
+// }
+//
+// const lyricsResults = async () => {
+//     await getLyrics()
+// }
+//
+//   searchResults()
+//   lyricsResults()
 
   let albumArt = indices[0].albumArt
   let title = indices[0].title
   let id = indices[0].id
+
+//     let albumArt = ' '
+//     let title = ' '
+//     let id = ' '
 
 // ******************************************************************************** //
 
