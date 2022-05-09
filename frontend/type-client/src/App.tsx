2cf4245eb69437127ac0b35f59a07d88b5340bf4
mstephe7 <matthew_stephens@brown.edu>
2022-05-09 11:14:57 -0400

    - song search working

--- frontend/type-client/src/App.tsx
+++ frontend/type-client/src/App.tsx
@@ -2,6 +2,11 @@ import React, {useState} from "react";
 import TypingPage from "./components/TypingPage";
 import "./styles.css";
 import Searcher from "./components/Searcher";
+import * as ReactBootStrap from 'react-bootstrap';
+// import Spinner from "./components/Spinner";
+
+// Good source for spinner: https://hashnode.com/post/how-to-add-loader-or-spinner-using-react-hooks-while-fetching-data-from-apis-ckhv38mr902kdgms100vh3u86
+// Look at all these cool spinners!: https://github.com/Mancancode/react-loader/tree/master/src/images/loaders
 
 var geniusLyricsAPI = require("genius-lyrics-api")
 
@@ -11,7 +16,6 @@ var lyrics = ' '
 var indices = []
 
 const options = {
-       //title:  document.getElementById("search"),
        title:  ' ',
        artist: ' ',
        apiKey: '3pTLkXn4VZpZXk3qJ61nkioYxvEaqzadxVGK1FNEN8-katQfRqngvvt1XzA06CaT', // Genius developer access token
@@ -19,7 +23,7 @@ const options = {
 }
 
 async function searchSongs() {
-    // @ts-ignoree
+    // @ts-ignore
     indices = await geniusLyricsAPI.searchSong(options)
     console.log(indices)
 }
@@ -40,6 +44,8 @@ async function getLyrics() {
 
 const App = () => {
   const [song, setSong] = useState<boolean>(false)
+  const [loading, setLoading] = useState(false) // NOTE: for the loader } don't even need this — song does the same thing
+
   const handleSearch = async () => {
     // Source: https://bobbyhadz.com/blog/typescript-type-null-is-not-assignable-to-type-string
     // options.title = document.getElementById("search").value !== null ? options.title: ' '
@@ -47,8 +53,10 @@ const App = () => {
     // @ts-ignore
     options.title = document.querySelector('input').value;
     console.log(options.title);
+
     await searchSongs()
     await getLyrics()
+    setLoading(true)
 
     // @ts-ignore
     albumArt = indices[0].albumArt
@@ -60,12 +68,18 @@ const App = () => {
     // @ts-ignore
     console.log(indices[0])
     setSong(true)
+
+    // setLoading(false) // NOTE: for the loader
   }
 
   return (
       <div className="App">
         {song ?
             <TypingPage id={id} title={title} lyrics={lyrics} albumArt={albumArt}/> :
+//             (<>
+//             <Searcher onLoad={handleSearch} />
+//             <ReactBootStrap.Spinner animation="border" />
+//             </>)}
             <Searcher onLoad={handleSearch} />}
       </div>
   );
